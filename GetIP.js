436f00d6f8409cba6cf3779acac5dd69b9cec364
jjeejj <wenjunjiang1993@163.com>
2018-10-17 14:10:36 +0800

    兼容 `Mac` , `Win` , `Linux` 平台

--- GetIP.js
+++ GetIP.js
@@ -1,5 +1,5 @@
-const os = require('os');
-const http = require('axios');
+const network = require('network');
+const { promisify } = require('util');
 const vscode = require('vscode');
 class GetIP {
     constructor(_vscode) {
@@ -11,33 +11,18 @@ class GetIP {
         //statusBar，是需要手动释放的
         this.statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
         let ips = await this.__getIp();
-        this.statusBar.text = '内网IP: '+ips.localIpV4;
+        console.log('ips',ips)
+        this.statusBar.text = '内网IP: '+ips.local;
         this.statusBar.tooltip = '公网IP: '+ips.public;
         this.statusBar.show();
     }
 
     async __getIp() {
         try{
-            // The code you place here will be executed every time your command is executed
-            // let log = vscode.window.createOutputChannel("getIp/log");
-            let networkInterfaces = os.networkInterfaces(); //网卡信息
-            let localEth0Info = {};
-            networkInterfaces.en0.forEach((value, index) => {
-                if(value.family.toLocaleLowerCase() == 'ipv4'){
-                    localEth0Info.ipv4 = value.address;
-                };
-                if(value.family.toLocaleLowerCase() == 'ipv6'){
-                    localEth0Info.ipv6 = value.address;
-                };
-            });
-
-            // let publicIp = ip.address();
-            let res = await http.get('http://ip.taobao.com/service/getIpInfo.php?ip=myip');
-            let publicIpInfo = res.data, publicIp = publicIpInfo.data.ip;
-
+            let localTp = await promisify(network.get_private_ip)();
+            let publicIp = await promisify(network.get_public_ip)();
             return {
-                localIpV4: localEth0Info.ipv4,
-                localIpV6: localEth0Info.ipv6,
+                local: localTp,
                 public: publicIp
             };
         }catch(err){
